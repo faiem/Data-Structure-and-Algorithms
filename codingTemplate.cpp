fb2b1f7193d1ca65bf09cdd30c47c24afe6c9f5d
faiem <faiem@overseasecurity.com>
2021-05-03 00:36:53 -0700

    Work plan for May 2021 Start brushing up knowledge on problem solving

---
+++ codingTemplate.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+
+const int MAX_N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+#define mp             make_pair
+#define pb             push_back
+#define fi             first
+#define se             second
+#define sz(x)          (int)((x).size())
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+int main()
+{
+
+    return 0;
+}
